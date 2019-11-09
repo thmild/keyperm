3aca2865220c06be60fbca51ba68e128e5b36791
mild <mild@zhaw.ch>
2019-11-09 15:39:26 +0100

    First version of package on public github repository

---
+++ R/create_ifl.R
@@ -0,0 +1,81 @@
+#' Create an Indexed Frequency List 
+#'
+#' The keyperm package stores frequency lists in a special data structure called
+#' indexed frequency list. This can currently be created from a tdm object as   
+#' implemented in the tm package.
+#'    
+#' Indexed frequency lists are essentially frequency lists stored in a three-column format,
+#' similar to the simple triplet matrix internally used by tm to store term-document-matrices.
+#' The first column stores number of document \code{i}, second number of term \code{j} and the third the 
+#' frequencies with which the term \code{j} occurs in document \code{i}. Zero occurences are omitted. 
+#' All columns contain integers, and the frequency list is sorted by document.  
+#'                   
+#' The object returned is of class \code{indexed_frequency_list}. In addition to the actual frequency 
+#' list it contains an index for fast access as well as pre-computed total number of tokens per
+#' document and total occurences per term. 
+#' 
+#' @param tdm a tdm-matrix from the tm package. Currently, this is the only supported input, but others may be added in later versions.
+#' @param subset_terms vector of terms to be considered. Can be integer (indices) or boolean. Terms not included still are counted for total number of token per document.
+#' @param subset_docs vector of documents to be considered. Can be integer (indices) or boolean. Documents excluded do not contribute to total number of occurences of a term. 
+#' @param corpus vector indicating which documents belong to corpus A (first corpus). Can be integer (indices) or boolean. Currently, only comparisons of two corpora are supported.
+#' @return A list with class \code{indexed_frequency_list} containing the following components:
+create_ifl <- function(tdm, 
+                       subset_terms = 1:dim(tdm)[1],
+                       subset_docs = 1:dim(tdm)[2],
+                       corpus) {
+
+  # save row and column totals so that they do not have to be recalculated 
+  #
+  # note that we include all terms in the  sums as we want total number of tokens per document
+  
+  tdm_rs <- slam::row_sums(tdm[subset_terms, subset_docs]) 
+  tdm_cs <- slam::col_sums(tdm[, subset_docs]) 
+  
+  ntotal <- sum(tdm_cs)
+  
+  # subsetting
+  
+  tdm <- tdm[subset_terms, subset_docs]  
+  
+  # convert to data frame 
+  
+  tdm_df <- data.frame(term = tdm$i, doc = tdm$j, freq = tdm$v)
+  tdm_df <- tdm_df[order(tdm_df$doc),] # sort by document
+  
+  doc_lookup <- aggregate(term ~ doc, tdm_df, length)
+  names(doc_lookup)[2] <- "nterms"
+  
+  # this is necessary so that we can handle empty documents
+  
+  doc_lookup <- merge(x = data.frame(doc = 1:dim(tdm)[2]), 
+                      y = doc_lookup,
+                      by = "doc",
+                      all = TRUE,
+                      sort = TRUE)
+  
+  doc_lookup$nterms[is.na(doc_lookup$nterms)] <- 0
+  
+  doc_lookup$start <- cumsum(doc_lookup$nterms) - doc_lookup$nterms + 1 # for R indexing convention
+  
+  # if corpus is a logical vectors, TRUE denotes corpus A 
+  # we convert to indices
+  #
+  # later versions may support a factor as input 
+  
+  if (is.logical(corpus)) corpus <- which(corpus)
+  
+  # create the actual indexed_frequency_list
+  # details may change in future versions
+  
+  ifl <- list(freqlist = tdm_df,
+              index = doc_lookup,
+              rowsums = tdm_rs,
+              colsums = tdm_cs,
+              ntotal = ntotal,
+              terms = dimnames(tdm)$Terms,
+              docs = dimnames(tdm)$Docs,
+              corp_A = corpus
+  )    
+  class(ifl) <- "indexed_frequency_list"
+  ifl
+}
---
+++ src/getPerm.cpp
@@ -0,0 +1,212 @@
+#include <RcppArmadillo.h>
+#include <RcppArmadilloExtensions/sample.h>
+// [[Rcpp::depends(RcppArmadillo)]]
+
+using namespace Rcpp;
+
+const int score_llr = 1;
+const int score_chisq = 2;
+const int score_diff = 3;
+const int score_logratio = 4;
+
+const int return_scores = 1;
+const int return_summary = 2;
+
+const double one_over_log2 = 1.442695040888963387;
+
+struct ABCD{
+  IntegerVector A;
+  IntegerVector B;
+  IntegerVector C;
+  IntegerVector D;
+  int NcorpA;
+  int NcorpB;
+};
+
+ABCD getABCD(IntegerVector ind, 
+             IntegerVector start_vek, 
+             IntegerVector nterm, 
+             IntegerVector freqs, 
+             IntegerVector termlist, 
+             IntegerVector rowsums, 
+             IntegerVector colsums, 
+             int ntotal) {
+  
+  int nt = rowsums.size();
+  int nind = ind.size();
+  int NcorpA = 0;
+  int NcorpB = 0;
+  
+  ABCD out;
+  
+  out.A = Rcpp::IntegerVector(nt);
+  out.B = Rcpp::IntegerVector(nt);
+  out.C = Rcpp::IntegerVector(nt);
+  out.D = Rcpp::IntegerVector(nt);
+  out.NcorpA = 0;
+  out.NcorpB = 0;
+  
+  for (int i = 0; i < nind; i ++) {
+    int currind = ind[i];
+    int currnterms = nterm[currind - 1];
+    int currstart = start_vek[currind - 1] - 1;
+    for (int j = 0; j < currnterms; j ++) {
+      int currterm = termlist[currstart + j];
+      out.A[currterm - 1] += freqs[currstart + j]; 
+    }
+    out.NcorpA += colsums[currind - 1];
+  }
+  
+  out.NcorpB = ntotal - out.NcorpA;
+  
+  for (int i = 0; i < nt; i ++) {
+    out.B[i] = out.NcorpA - out.A[i];
+    out.C[i] = rowsums[i] - out.A[i];
+    out.D[i] = ntotal - rowsums[i] - out.B[i];
+  }
+  
+  return out;
+}
+
+// [[Rcpp::export]]
+Rcpp::List getABCD2(IntegerVector ind, 
+                    IntegerVector start_vek, 
+                    IntegerVector nterm, 
+                    IntegerVector freqs, 
+                    IntegerVector termlist, 
+                    IntegerVector rowsums, 
+                    IntegerVector colsums, 
+                    int ntotal) {
+  ABCD abcd = getABCD(ind, 
+                      start_vek, 
+                      nterm, 
+                      freqs, 
+                      termlist, 
+                      rowsums, 
+                      colsums, 
+                      ntotal);
+  return Rcpp::List::create(Rcpp::Named("A") = abcd.A,
+                            Rcpp::Named("B") = abcd.B,
+                            Rcpp::Named("C") = abcd.C,
+                            Rcpp::Named("D") = abcd.D,
+                            Rcpp::Named("NcorpA") = abcd.NcorpA,
+                            Rcpp::Named("NcorpB") = abcd.NcorpB);
+  
+}
+
+//Rcpp::List getA(IntegerVector ind, 
+
+// [[Rcpp::export]]
+NumericVector getScores(IntegerVector ind, 
+                        IntegerVector start_vek, 
+                        IntegerVector nterm, 
+                        IntegerVector freqs, 
+                        IntegerVector termlist, 
+                        IntegerVector rowsums, 
+                        IntegerVector colsums, 
+                        int ntotal,
+                        int scoretype,
+                        double laplace = 0.0) {
+  
+  NumericVector out;
+  
+  ABCD abcd = getABCD(ind, 
+                      start_vek, 
+                      nterm, 
+                      freqs, 
+                      termlist, 
+                      rowsums, 
+                      colsums, 
+                      ntotal);
+  
+  NumericVector Anum = as<NumericVector>(abcd.A);
+  NumericVector Bnum = as<NumericVector>(abcd.B);
+  NumericVector Cnum = as<NumericVector>(abcd.C);
+  NumericVector Dnum = as<NumericVector>(abcd.D);
+  double NcorpA = static_cast<double>(abcd.NcorpA);
+  double NcorpB = static_cast<double>(abcd.NcorpB);
+  
+  if (scoretype == score_llr) {
+    out = (-2) * (ifelse(Anum == 0, 0, Anum*log((Anum + Bnum)*(Anum + Cnum)/Anum)) + 
+      ifelse(Bnum == 0, 0, Bnum*log((Anum + Bnum)*(Bnum + Dnum)/Bnum)) +
+      ifelse(Cnum == 0, 0, Cnum*log((Cnum + Dnum)*(Anum + Cnum)/Cnum)) +
+      ifelse(Dnum == 0, 0, Dnum*log((Bnum + Dnum)*(Cnum + Dnum)/Dnum)) - 
+      ntotal*log(ntotal)
+    );
+  } else if (scoretype == score_chisq) {  // this cannot cope with zero column sums, probably...
+    out = ifelse(rowsums ==0, 
+                 0, 
+                 ntotal * (Anum * Dnum - Bnum * Cnum) *  (Anum * Dnum - Bnum * Cnum) / ((Anum + Bnum) * (Anum + Cnum) * (Cnum + Dnum) * (Bnum + Dnum)) ); 
+  } else if (scoretype == score_diff) {  
+    out = Anum / NcorpA  - Cnum / NcorpB;   
+  } else if (scoretype == score_logratio) {  
+    out = one_over_log2 * log((Anum + laplace) / (NcorpA  + laplace))  - one_over_log2 *log((Cnum  + laplace)/ (NcorpB  + laplace));   
+  }
+  
+  
+  return(out);
+}
+
+// [[Rcpp::export]]
+
+NumericMatrix genPerm(IntegerVector ind, 
+                      IntegerVector start_vek, 
+                      IntegerVector nterm, 
+                      IntegerVector freqs, 
+                      IntegerVector termlist, 
+                      IntegerVector rowsums, 
+                      IntegerVector colsums, 
+                      int ntotal,
+                      int nperm,
+                      int output,
+                      int scoretype,
+                      NumericVector observed,
+                      double laplace = 0.0) {
+  
+  int nt = rowsums.size();
+  int ndoc = colsums.size();
+  int ndocA = ind.size();
+  
+  // output sizes depends on what type of output we want
+  
+  int ncols_out;
+  
+  if (output == return_scores) 
+    ncols_out = nperm;
+  else
+    ncols_out = 3;
+  
+  NumericMatrix out(nt, ncols_out);
+  
+  NumericVector prob = NumericVector::create();
+  IntegerVector x =  seq(1, ndoc);
+  IntegerVector ind2;
+  
+  for (int j = 0; j < nperm; j++) {
+    ind2 = RcppArmadillo::sample(x, ndocA, FALSE, prob);
+    NumericVector G = getScores(ind2, start_vek, nterm, freqs, termlist, rowsums, colsums, ntotal, scoretype, laplace);
+    
+    // do calculations conditional on whether we want the full results (1) or just a summary (# <, # =,  #>) (2)
+    
+    if (output == return_scores) {
+      for (int i = 0; i < nt; i++) {
+        out(i, j) = G[i];
+      }
+    }
+    else{
+      for (int i = 0; i < nt; i++) {
+        double scorediff = observed[i] - G[i];
+        if (std::fabs(scorediff) <= 2 * std::numeric_limits<double>::epsilon()) 
+          out(i, 1) += 1;
+        else if (scorediff > 0) 
+          out(i, 0) += 1;
+        else 
+          out(i, 2) += 1;
+      }
+      
+    }
+    
+  }
+  
+  return out;
+}
