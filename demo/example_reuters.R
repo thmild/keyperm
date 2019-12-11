14120b54449b7903c61ac224f7ffc0ff9f27db78
thmild <mild@zhaw.ch>
2019-12-11 15:23:59 +0100

    Added pre-screening example for use of combine_results() to Reuters demo

--- demo/example_reuters.R
+++ demo/example_reuters.R
@@ -42,3 +42,28 @@ logratio <- keyness_scores(reuters_ifl, type = "logratio", laplace = 1)
 keyp2 <- keyperm(reuters_ifl, logratio, type = "logratio", 
                 laplace = 1, output = "counts", nperm = 1000)
 head(p_value(keyp2, alternative = "greater"), n = 10)
+
+# it may be of interest to improve accuracy of the small p-values. 
+# Think of this in terms of spending the computational budget mainly 
+# on the terms for which higher accuracy matters most 
+
+pvals <- p_value(keyp2, alternative = "greater")
+table(pvals > 0.1)
+
+small_p <- which(pvals < 0.1)
+
+logratio_subset <- logratio[small_p]
+reuters_ifl_subset <- create_ifl(tdm, subset_terms = small_p, corpus = corpus)
+
+keyp2_subset <- keyperm(reuters_ifl_subset, logratio_subset, type = "logratio", 
+                 laplace = 1, output = "counts", nperm = 9000)
+
+# combine counts from both runs using the combiner
+
+keyp2_combined <- combine_results(keyp2, keyp2_subset)
+
+# smaller p-values are based on 10000, the larger ones on 10000 random permutations
+# note that 10000 is still far too small for real applications
+
+head(p_value(keyp2_combined, alternative = "greater"), n = 10)
+
